ca7f41e2ceed591cf7ae7df3615e06e2a208aef0
andres ibanez <andresibanezulloa@gmail.com>
2023-11-28 08:35:48 -0300

    search

--- javascript/application.js
+++ javascript/application.js
@@ -1,6 +1,8 @@
 import { Application, Controller } from 'stimulus';
+import DisableButtonController from "./controllers/disable_button_controller.js";
+
+
 window.Stimulus = Application.start()
 
-import DisableButtonController from "./controllers/disable_button_controller.js";
 Stimulus.register("disable-button", DisableButtonController);
 
--- javascript/controllers/disable_button_controller.js
+++ javascript/controllers/disable_button_controller.js
@@ -1,13 +1,20 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-
+    static targets = ["button", "link"]
     connect() {
         console.log("Hello from controller");
     }
 
     disable() {
-        this.element.setAttribute("disabled","")
-        this.element.innerText = "Bingo"
+        this.buttonTarget.setAttribute("disabled","")
+        this.buttonTarget.innerText = "Bingo"
+        this.linkTarget.classList.remove("d-none")
+    }
+
+    reset() {
+        this.linkTarget.classList.add("d-none")
+        this.buttonTarget.removeAttribute("disabled","")
+        this.buttonTarget.innerText = "Click me!"
     }
 }
